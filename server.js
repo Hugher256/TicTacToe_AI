c01a2bb55b3f7b78844fffe1a56fc157a8a471c2
Anshit Verma <anshitverma55@gmail.com>
2020-11-05 22:37:07 +0530

    Final Commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const dataStore = require('nedb')
+
+const app = express()
+
+const dataBase = new dataStore('database.db', { autoload: true })
+dataBase.loadDatabase()
+
+app.listen(process.env.PORT, () => {
+	console.log('Listening')
+})
+
+app.use(express.static('public'))
+app.use(express.json({ limit: '100kb' }))
+
+app.post('/name', (request, response) => {
+	const data = request.body
+	console.log(data)
+	dataBase.find({ name: data.name }, (err, docs) => {
+		if (docs.length) {
+			dataBase.update(
+				{ name: data.name },
+				{ name: data.name, matches: data.matches }
+			)
+			dataBase.loadDatabase()
+			console.log('Updated!')
+		} else {
+			dataBase.insert(data)
+			console.log('Inserted!')
+		}
+	})
+	response.end()
+	//response.json({ status: "Success" });
+})
+
+app.get('/name', (request, response) => {
+	dataBase
+		.find({})
+		.sort({ matches: -1 })
+		.exec((err, data) => {
+			if (err) {
+				response.end()
+				return
+			}
+			response.json(data)
+		})
+})
